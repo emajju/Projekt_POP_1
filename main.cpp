f278b94d1013fc5e7292e7f89cbc789d02ae6710
Jan Mironkiewicz <janmironkiewicz@gmail.com>
2016-11-14 21:51:03 +0100

    Coraz więcej funkcji, powijaki losowości z tablicy

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 /*
 *   Program realizujący grę typu wykreślanka
 *   Autor Jan Mironkiewcz s165233
+*   Główny słownik Sjp do gier
 */
 #include <iostream>
 #include <cstdlib>
 #include <string>
 #include <time.h>
+#include <fstream>
 
 using namespace std;
 
@@ -26,21 +28,65 @@ void wypisz_autor(void)
     cout <<"naciśnij klawisz aby wrócić do menu"<<endl;
     system("pause");
 }
+void menu_slowniki(void)
+{
+    //TODO wczytanie i zarządzanie słownikami, prawdopodobnie potrzebny będzie wskaźnik do tablicy słownika
+
+}
+
 int main()
 {
-    //Wypisujemy menu do konsoli
-    cout << "Witamy w grze WYKREŚLANKA!\n";
-    cout << "Wybierz co chcesz robić dalej: (wpisz liczbę i naciśnij enter)\n";
+    //przygotowanie słownika docelowo zapakować do funkcji
+    ifstream slownik;
+    slownik.open("slowa.txt");
+    if(!slownik.good())
+    {
+        cout <<"Nie wczytano słownika";
+        system("pause");
+        return 0;
+    }
+    uint64_t liczba_linii = 0;//64bitowa ponieważ lini może być bardzo dużo(obszerny słownik)
+    string linia;
+    while(!slownik.eof())
+        {
+            getline(slownik,linia);
+            liczba_linii++;
+        }
+    slownik.close();
+    slownik.open("slowa.txt");
+    cout <<"lini: "<<liczba_linii<<endl;
+    //policzmy ilość linii
+    string *tablica = new string[liczba_linii];//mamy dużo pamięci w komputerze więc będziemy z tego korzystać
+    liczba_linii=0; //tą część trzeba zamienić na przejście na początek pliku zamiast zamykania i otwierania
+    while(!slownik.eof())
+        {
+            getline(slownik,tablica[liczba_linii]);
+            liczba_linii++;
+        }
 
-
-
+    string *mniejsza = new string[1000];
+    srand(time(NULL ) ); //podajemy czas jako ziarno
+    uint64_t liczba= 1;
+    for(int i = 0; i<999; i++)
+    {
+        liczba = rand()*(rand()%81);
+        mniejsza[i] = tablica[liczba];
+        cout << mniejsza[i]<<endl;
+        //dodać ograniczenia co do długości liter tj więcej niż 3 mniej niż wyjdzie z wielkości planszy
+    }
+    delete [] tablica;
 
     while(1) //Pętla nieskończona umożliwia grę bez końca oraz poruszanie po menu
     {
+        //Wypisujemy menu do konsoli
+        cout << "Witamy w grze WYKREŚLANKA!\n";
+        cout << "Wybierz co chcesz robić dalej: (wpisz liczbę i naciśnij enter)\n";
+
         int poziom_menu = 0;
         wypisz_menu();
         cin >> poziom_menu;
         //TODO błędy wprowadzania typu zły znak
+
 
         switch(poziom_menu)
         {
@@ -48,7 +94,13 @@ int main()
 
                 break;
         case 2: //Wywolanie instrukcji
-
+                system("cls");
+                cout <<"Gra nie zwraca uwagi na wielkość liter"<<endl;
+                cout <<"Można wprowadzić więcej niż jedno słowo za jednym razem. Wystarczy rozdzielić je spacją"<<endl;
+                cout <<"Słów należy szukać w pionie, poziomie oraz na ukos"<<endl;
+                cout <<"Jeżeli chcesz przerwać swoją grę wpisz '_Koniec_'"<<endl;
+                cout <<"Nacisnij klawisz aby powrócić do menu...";
+                system("pause");
                 break;
         case 3: //Docelowe menu zarządzania słownikami do generowania
 
@@ -57,6 +109,7 @@ int main()
                 wypisz_autor();
                 break;
         case 5://Wyjście z programu
+
                 return 0;
                 break;
         default:
